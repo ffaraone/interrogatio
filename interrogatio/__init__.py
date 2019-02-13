bbaf3abf6c97d2f8e7a141054bc6cfe3e3ab891f
Francesco Faraone <ffaraone@gaa9.com>
2019-02-13 10:25:48 +0100

    beta 3

--- interrogatio/__init__.py
+++ interrogatio/__init__.py
@@ -3,7 +3,7 @@ from .utils.constants import InputMode
 from .utils.registries import (get_input_handlers_registry,
                                get_validators_registry)
 
-__version__ = '1.0.0b2'
+__version__ = '1.0.0b3'
 
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
 
